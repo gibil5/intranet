7f736edc4ff8693bd23c0f060ad280c9ac04c56c
gibil <jrevilla55@gmail.com>
2018-12-16 15:07:22 -0500

    ..

--- Gemfile
+++ Gemfile
@@ -1,9 +1,15 @@
 #source 'https://rubygems.org'
 source 'http://rubygems.org'
 
-
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
+
+
+
+# Image upload
+gem 'cloudinary'
+
+
 
 
 # Use sqlite3 as the database for Active Record
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,27 @@ class StaticPagesController < ApplicationController
   end
 
   def reports
-  end
 
+  	# Urls
+
+  	# Cloudinary
+	#@pic_url_1 = "https://res.cloudinary.com/dam0dmleq/image/upload/v1544988332/Chavarri/fig_pie_year_2018.png"
+	#@pic_url_2 = "https://res.cloudinary.com/dam0dmleq/image/upload/v1544988345/Chavarri/fig_pie_year_2018_micro.png"
+	#@pic_url_3 = "https://res.cloudinary.com/dam0dmleq/image/upload/v1544988302/Chavarri/fig_line_year_2018.png"
+	#@pic_url_4 = "https://res.cloudinary.com/dam0dmleq/image/upload/v1544988317/Chavarri/fig_line_year_micro_2018.png"
+
+  	# Docean
+	@pic_url_1 = "http://165.227.25.8/reports/fig_pie_year_2018.png"
+	@pic_url_2 = "http://165.227.25.8/reports/fig_pie_year_2018_micro.png"
+	@pic_url_3 = "http://165.227.25.8/reports/fig_line_year_2018.png"
+	@pic_url_4 = "http://165.227.25.8/reports/fig_line_year_micro_2018.png"
+
+
+
+
+  	# Titles
+	@pic_txt_1 = "Ventas Anual 2018"
+
+  end
 
 end
